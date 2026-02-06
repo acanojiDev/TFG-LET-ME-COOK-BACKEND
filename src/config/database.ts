a655698777c8ba956210296d6a44dd05ffc852bc
acanojiDev <aantoniiocano@gmail.com>
2026-02-06 14:29:33 +0100

    Limpieza de archivos y ordenación de estructura

--- src/config/database.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-console.log(process.env.DATABASE_URL);
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL
-});
-export const prisma = new PrismaClient({ adapter });
